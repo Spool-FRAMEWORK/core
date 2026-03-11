aceb0c7f74c5203f85544047e9801b60d64c7e97
javcastilla <javier.casno@gmail.com>
2026-03-11 22:22:54 +0000

    refactor: imported domain mappers from crawler module

---
+++ src/main/java/software/spool/core/utils/DomainEventMapping.java
@@ -0,0 +1,35 @@
+package software.spool.core.utils;
+
+import software.spool.core.model.Event;
+import software.spool.core.model.IdempotencyKey;
+import software.spool.core.port.PayloadDeserializer;
+
+import java.util.function.BiFunction;
+
+public final class DomainEventMapping<D> {
+    private final PayloadDeserializer<D> deserializer;
+    private final BiFunction<D, IdempotencyKey, Event> toEvent;
+
+    private DomainEventMapping(PayloadDeserializer<D> deserializer,
+                               BiFunction<D, IdempotencyKey, Event> toEvent) {
+        this.deserializer = deserializer;
+        this.toEvent = toEvent;
+    }
+
+    public static <E extends Event> DomainEventMapping<E> of(
+            PayloadDeserializer<E> deserializer) {
+        return new DomainEventMapping<>(deserializer, (dto, key) -> dto);
+    }
+
+    public static <D> DomainEventMapping<D> of(
+            PayloadDeserializer<D> deserializer,
+            BiFunction<D, IdempotencyKey, Event> toEvent) {
+        return new DomainEventMapping<>(deserializer, toEvent);
+    }
+
+    public Event resolve(String payload, IdempotencyKey key) {
+        D dto = deserializer.deserialize(payload);
+        return toEvent.apply(dto, key);
+    }
+}
+
---
+++ src/main/java/software/spool/core/utils/NamingConvention.java
@@ -0,0 +1,8 @@
+package software.spool.core.utils;
+
+public enum NamingConvention {
+    CAMEL_CASE,
+    SNAKE_CASE,
+    PASCAL_CASE,
+    KEBAB_CASE
+}

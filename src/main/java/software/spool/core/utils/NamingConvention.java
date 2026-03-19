6beb71b018f9b9220f295ab5f9a629076c528aad
javcastilla <javier.casno@gmail.com>
2026-03-19 21:57:39 +0000

    refactor: name convention now uses factory within its values

--- src/main/java/software/spool/core/utils/NamingConvention.java
+++ src/main/java/software/spool/core/utils/NamingConvention.java
@@ -1,5 +1,8 @@
 package software.spool.core.utils;
 
+import software.spool.core.infrastructure.adapter.DomainMapperFactory;
+import software.spool.core.port.PayloadDeserializer;
+
 /**
  * JSON property naming strategies used when deserializing domain events.
  *
@@ -12,5 +15,14 @@ public enum NamingConvention {
     CAMEL_CASE,
     SNAKE_CASE,
     PASCAL_CASE,
-    KEBAB_CASE
+    KEBAB_CASE;
+
+    public <D> PayloadDeserializer<D> deserializerFor(Class<D> type) {
+        return switch (this) {
+            case SNAKE_CASE  -> DomainMapperFactory.snakeCase(type);
+            case CAMEL_CASE  -> DomainMapperFactory.camelCase(type);
+            case PASCAL_CASE -> DomainMapperFactory.pascalCase(type);
+            case KEBAB_CASE  -> DomainMapperFactory.kebabCase(type);
+        };
+    }
 }

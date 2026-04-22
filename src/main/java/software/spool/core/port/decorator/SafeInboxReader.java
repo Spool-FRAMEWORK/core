85fe7e42ee669ff3813c9e1b8504c11808692260
javcastilla <javier.casno@gmail.com>
2026-04-22 20:14:32 +0100

    delete: removed sql inbox adapters

--- src/main/java/software/spool/core/port/decorator/SafeInboxReader.java
+++ src/main/java/software/spool/core/port/decorator/SafeInboxReader.java
@@ -2,10 +2,12 @@ package software.spool.core.port.decorator;
 
 import software.spool.core.exception.InboxReadException;
 import software.spool.core.exception.SpoolException;
+import software.spool.core.model.vo.IdempotencyKey;
 import software.spool.core.model.vo.InboxItem;
 import software.spool.core.model.InboxItemStatus;
 import software.spool.core.port.inbox.InboxReader;
 
+import java.util.Optional;
 import java.util.stream.Stream;
 
 /**
@@ -23,6 +25,17 @@ public class SafeInboxReader implements InboxReader {
     public Stream<InboxItem> findByStatus(InboxItemStatus status) {
         try {
             return reader.findByStatus(status);
+        } catch (SpoolException e) {
+            throw e;
+        } catch (Exception e) {
+            throw new InboxReadException(e.getMessage(), e);
+        }
+    }
+
+    @Override
+    public Optional<InboxItem> getBy(IdempotencyKey idempotencyKey) throws InboxReadException {
+        try {
+            return reader.getBy(idempotencyKey);
         } catch (SpoolException e) {
             throw e;
         } catch (Exception e) {

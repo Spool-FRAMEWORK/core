b8a60f45fcf0127a5b4a0fe9c487af9e0d7f4624
javcastilla <javier.casno@gmail.com>
2026-04-26 16:09:03 +0100

    refactor: updated event broker adapter to follow new signature

--- src/main/java/software/spool/core/adapter/kafka/KafkaEventBroker.java
+++ src/main/java/software/spool/core/adapter/kafka/KafkaEventBroker.java
@@ -6,21 +6,21 @@ import software.spool.core.model.Event;
 import software.spool.core.port.bus.*;
 
 public class KafkaEventBroker implements EventBroker {
-    private final EventPublisher emitter;
-    private final EventSubscriber listener;
+    private final EventPublisher publisher;
+    private final EventSubscriber subscriber;
 
-    public KafkaEventBroker(EventPublisher emitter, EventSubscriber listener) {
-        this.emitter = emitter;
-        this.listener = listener;
+    public KafkaEventBroker(EventPublisher publisher, EventSubscriber subscriber) {
+        this.publisher = publisher;
+        this.subscriber = subscriber;
     }
 
     @Override
-    public void publish(Event event) throws EventBrokerEmitException {
-        emitter.publish(event);
+    public <E extends Event> void publish(Destination destination, BrokerMessage<E> message) throws EventBrokerEmitException {
+        publisher.publish(destination, message);
     }
 
     @Override
-    public <E extends Event> Subscription on(Class<E> event, Handler<E> handler) throws EventBrokerListenException {
-        return listener.on(event, handler);
+    public <E extends Event> Subscription subscribe(Destination destination, Class<E> eventType, Handler<E> handler) throws EventBrokerListenException {
+        return subscriber.subscribe(destination, eventType, handler);
     }
 }

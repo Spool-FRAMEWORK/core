b8f68a01b828d94486491cd03875e786bf5f5e66
javcastilla <javier.casno@gmail.com>
2026-04-28 23:36:44 +0100

    feat: implemented method to update several envelopes at the same time

--- src/main/java/software/spool/core/port/inbox/InboxUpdater.java
+++ src/main/java/software/spool/core/port/inbox/InboxUpdater.java
@@ -5,9 +5,15 @@ import software.spool.core.model.EnvelopeStatus;
 import software.spool.core.model.vo.Envelope;
 import software.spool.core.model.vo.IdempotencyKey;
 
+import java.util.Collection;
+import java.util.List;
+
 public interface InboxUpdater {
     default Envelope update(Envelope envelope) throws InboxUpdateException {
         return update(envelope.idempotencyKey(), envelope.status());
     };
-    Envelope update(IdempotencyKey idempotencyKey, EnvelopeStatus status) throws InboxUpdateException;
+    default Envelope update(IdempotencyKey idempotencyKey, EnvelopeStatus status) throws InboxUpdateException {
+        return update(List.of(idempotencyKey), status).stream().findFirst().orElse(null);
+    };
+    Collection<Envelope> update(Collection<IdempotencyKey> idempotencyKeys, EnvelopeStatus status) throws InboxUpdateException;
 }

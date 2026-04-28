af02cef7414f93f8fd35426a4710e7db5b864782
javcastilla <javier.casno@gmail.com>
2026-04-28 23:07:40 +0100

    feat: implemented method at inbox updater to update status only

--- src/main/java/software/spool/core/port/inbox/InboxUpdater.java
+++ src/main/java/software/spool/core/port/inbox/InboxUpdater.java
@@ -1,8 +1,11 @@
 package software.spool.core.port.inbox;
 
 import software.spool.core.exception.InboxUpdateException;
+import software.spool.core.model.EnvelopeStatus;
 import software.spool.core.model.vo.Envelope;
+import software.spool.core.model.vo.IdempotencyKey;
 
 public interface InboxUpdater {
     Envelope update(Envelope envelope) throws InboxUpdateException;
+    Envelope update(IdempotencyKey idempotencyKey, EnvelopeStatus status) throws InboxUpdateException;
 }

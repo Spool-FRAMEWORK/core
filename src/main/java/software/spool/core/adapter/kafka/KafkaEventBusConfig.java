55730eb8f011220f7b61cdb1f295ad253a307d43
javcastilla <javier.casno@gmail.com>
2026-05-01 14:40:26 +0100

    refactor!: moved infrastructure to its proper module

--- src/main/java/software/spool/core/adapter/kafka/KafkaEventBusConfig.java
+++
@@ -1,7 +0,0 @@
-package software.spool.core.adapter.kafka;
-
-public record KafkaEventBusConfig(String bootstrapServers) {
-    public static KafkaEventBusConfig local() {
-        return new KafkaEventBusConfig("localhost:9092");
-    }
-}

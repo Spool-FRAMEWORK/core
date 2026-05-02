74b43fcd272ed02e5ab0aea3b09f07ac45adfe08
javcastilla <javier.casno@gmail.com>
2026-05-02 18:48:00 +0100

    fix: corrected store adapter

--- src/main/java/software/spool/core/circuitbreaker/adapter/InMemoryCircuitBreakerStateStore.java
+++ src/main/java/software/spool/core/circuitbreaker/adapter/InMemoryCircuitBreakerStateStore.java
@@ -1,8 +1,13 @@
 package software.spool.core.circuitbreaker.adapter;
 
 import software.spool.core.circuitbreaker.model.CircuitBreakerState;
+import software.spool.core.circuitbreaker.model.CircuitBreakerSnapshot;
+import software.spool.core.circuitbreaker.model.CircuitBreakerStatus;
 import software.spool.core.circuitbreaker.port.CircuitBreakerStateStore;
 
+import java.time.Instant;
+import java.util.ArrayDeque;
+import java.util.Optional;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 
@@ -11,24 +16,24 @@ public class InMemoryCircuitBreakerStateStore implements CircuitBreakerStateStor
 
     @Override
     public CircuitBreakerState load(String id) {
-        return store.get(id);
+        return store.computeIfAbsent(id, this::initialState);
     }
 
     @Override
     public boolean compareAndSet(long expectedVersion, CircuitBreakerState newState) {
-        return store.compute(newState.snapshot().id(), (id, currentState) -> {
-            if (currentState == null) {
-                CircuitBreakerState result = null;
-                if (expectedVersion == 0)
-                    result = newState;
-                return result;
-            }
-
-            if (currentState.snapshot().version() == expectedVersion) {
+        store.computeIfAbsent(newState.snapshot().id(), this::initialState);
+        return store.compute(newState.snapshot().id(), (key, currentState) -> {
+            if (currentState == null || currentState.snapshot().version() == expectedVersion) {
                 return newState;
-            } else {
-                return currentState;
             }
+            return currentState;
         }) == newState;
+    }
+
+    private CircuitBreakerState initialState(String id) {
+        CircuitBreakerSnapshot snap = new CircuitBreakerSnapshot(
+                id, 0, 0, 0, Instant.now(), new ArrayDeque<>(), 0L
+        );
+        return new CircuitBreakerState(snap, CircuitBreakerStatus.CLOSED, Optional.empty());
     }
 }

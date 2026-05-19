982ba3bb93bb62e89a6b45ed352f02443912d224
javcastilla <javier.casno@gmail.com>
2026-05-19 12:45:46 +0100

    refactor: event metadata now stores byte[] instead of String

--- src/main/java/software/spool/core/model/vo/EventMetadata.java
+++ src/main/java/software/spool/core/model/vo/EventMetadata.java
@@ -4,22 +4,22 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 
-public record EventMetadata(Map<EventMetadataKey, String> metadata) {
+public record EventMetadata(Map<EventMetadataKey, byte[]> metadata) {
 
     public EventMetadata() {
         this(new HashMap<>());
     }
 
-    public String get(final EventMetadataKey key) {
+    public byte[] get(final EventMetadataKey key) {
         return metadata.get(key);
     }
 
-    public EventMetadata set(final EventMetadataKey key, final String value) {
+    public EventMetadata set(final EventMetadataKey key, final byte[] value) {
         metadata.put(key, value);
         return this;
     }
 
-    public Set<Map.Entry<EventMetadataKey, String>> entrySet() {
+    public Set<Map.Entry<EventMetadataKey, byte[]>> entrySet() {
         return metadata.entrySet();
     }
 }
